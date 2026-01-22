7e5a3b04106aa72d14cbca58209c933fb8a7fab7
David <97206601+davidsahani@users.noreply.github.com>
2026-01-22 22:26:15 +0530

    refactor(result): remove explicit Result constructors and helper tags

--- src/Utils/Result.hpp
+++ src/Utils/Result.hpp
@@ -4,29 +4,6 @@
 #include <variant>
 #include <stdexcept>
 
-namespace result::private_ {
-
-template <typename T>
-struct OkTag { T value; };
-
-template <typename E>
-struct ErrTag { E error; };
-
-// Traits to detect any OkTag<*> / ErrTag<*>
-template <class X> struct is_ok_tag  : std::false_type {};
-template <class U> struct is_ok_tag<OkTag<U>> : std::true_type {};
-
-template <class X> struct is_err_tag : std::false_type {};
-template <class V> struct is_err_tag<ErrTag<V>> : std::true_type {};
-
-template <class X>
-inline constexpr bool is_ok_tag_v = is_ok_tag<std::remove_cvref_t<X>>::value;
-
-template <class X>
-inline constexpr bool is_err_tag_v = is_err_tag<std::remove_cvref_t<X>>::value;
-
-} // namespace result::private_
-
 template <typename T, typename E>
 class Result {
     static_assert(!std::is_same_v<T, E>,
@@ -35,44 +12,22 @@ class Result {
     );
 
 public:
-    // OK (implicit constructor): constructible as T, not constructible as E
+    // OK constructor: constructible as T, not constructible as E
     template <class U>
     requires (
         std::is_constructible_v<T, U&&> &&
         !std::is_same_v<std::remove_cvref_t<U>, Result> &&
-        !result::private_::is_ok_tag_v<U> &&
-        !result::private_::is_err_tag_v<U> &&
         !std::is_same_v<std::remove_cvref_t<U>, E>
     )
     Result(U &&value) : _data(std::in_place_type<T>, std::forward<U>(value)) {}
 
-    // ERR (implicit constructor): constructible as E, not constructible as T
+    // ERR constructor: constructible as E, not constructible as T
     template <class U>
     requires (
         std::is_same_v<std::remove_cvref_t<U>, E> &&
-        !std::is_same_v<std::remove_cvref_t<U>, Result> &&
-        !result::private_::is_ok_tag_v<U> &&
-        !result::private_::is_err_tag_v<U>
+        !std::is_same_v<std::remove_cvref_t<U>, Result>
     )
     Result(U &&error) : _data(std::in_place_type<E>, std::forward<U>(error)) {}
-
-    // OK: Explicit constructor for OkTag<U>
-    template <class U> requires std::is_constructible_v<T, U&&>
-    Result(result::private_::OkTag<U>&& ok) : _data(std::in_place_type<T>, std::forward<U>(ok.value)) {}
-
-    // ERR: Explicit constructor for ErrTag<V>
-    template <class V> requires std::is_same_v<std::remove_cvref_t<V>, E>
-    Result(result::private_::ErrTag<V>&& err) : _data(std::in_place_type<E>, std::forward<V>(err.error)) {}
-
-    // OK: Static named constructor
-    [[nodiscard]] static Result Ok(T value) {
-        return Result(std::move(value), ok_ctor_t{});
-    }
-
-    // ERR: Static named constructor
-    [[nodiscard]] static Result Err(E error) {
-        return Result(std::move(error), err_ctor_t{});
-    }
 
     // Check if result is success
 	bool has_value() const {
@@ -105,6 +60,17 @@ public:
 		return std::get<E>(_data);
 	}
 
+	// Returns the value if successful, otherwise returns the default value
+    template <class U>
+    constexpr T value_or(U&& default_value) const& {
+        return has_value() ? std::get<T>(_data) : static_cast<T>(std::forward<U>(default_value));
+    }
+
+    template <class U>
+    constexpr T value_or(U&& default_value) && {
+        return has_value() ? std::move(std::get<T>(_data)) : static_cast<T>(std::forward<U>(default_value));
+    }
+
     // Pattern matching-like API
 	template <typename OkFn, typename ErrFn>
 	auto match(OkFn okFn, ErrFn errFn) const {
@@ -116,23 +82,4 @@ public:
 
 private:
     std::variant<T, E> _data;
-
-    struct ok_ctor_t {};
-    struct err_ctor_t {};
-
-    // Private constructors
-    explicit Result(T &&value, ok_ctor_t) : _data(std::in_place_type<T>, std::move(value)) {}
-    explicit Result(E &&error, err_ctor_t) : _data(std::in_place_type<E>, std::move(error)) {}
 };
-
-namespace result {
-	template <typename T>
-	[[nodiscard]] private_::OkTag<std::decay_t<T>> Ok(T &&value) {
-		return private_::OkTag<std::decay_t<T>>{std::forward<T>(value)};
-	}
-
-	template <typename E>
-	[[nodiscard]] private_::ErrTag<std::decay_t<E>> Err(E &&error) {
-		return private_::ErrTag<std::decay_t<E>>{std::forward<E>(error)};
-	}
-} // namespace result
